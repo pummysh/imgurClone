fa0b2ced666a2b93fdf51da34abf32dee3f13a99
PUMMY SHARMA <pummy9799@gmail.com>
2022-01-28 23:18:23 +0530

    debouncing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var timeId;
+let list_box=document.getElementById("search-items");
 
 fetch("https://fakestoreapi.com/products")
     .then(response => response.json())
@@ -21,3 +23,49 @@ function showData(data) {
         container.append(div);
     })
 }
+
+function debounce(func,delay) {
+    
+    if(timeId){
+        clearInterval(timeId);
+    }
+
+    timeId=setTimeout(()=>{
+        func();
+    },delay)
+}
+
+async function main(){
+    let name=document.getElementById("in").value;
+    if(name.length>0){
+        
+        let res=await search(name);
+        let data=res.Search;
+        listAppend(data);
+    }else{
+        list_box.style.visibility="hidden";
+    }
+
+}
+
+async function search(name){
+    try{
+        let res = await fetch(`https://www.omdbapi.com/?s=${name}&apikey=4b125d6`);
+        let data=await res.json();
+        return data;
+    }catch(e){
+        console.log("Error: " + e)
+    }
+}
+
+function listAppend(data){
+    list_box.style.visibility="visible";
+    list_box.innerHTML=null;
+    data.forEach(a=>{
+        let list=document.createElement("li");
+        list.textContent =a.Title;
+        list_box.append(list)
+    })
+
+}
+
